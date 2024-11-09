3b322f288616498183f9032aae42b8115e6cb103
Suvajit Patra <suvajit790@gmail.com>
2024-11-09 14:00:54 +0530

    https removed

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -15,3 +15,9 @@ bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:5000')
 forwarded_allow_ips = '*'
 
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+
+
+
+
+# gunicorn --config gunicorn_config.py app:app
+# ssh -N -f -p 4999 -L localhost:5000:localhost:5000 recognition-app-run@astra
